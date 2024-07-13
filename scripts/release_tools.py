0f24a484a089d9fb000f13f0036bd0dd50cef561
mpazaryna <mpazbot@gmail.com>
2024-07-13 10:05:29 -0400

    fix: update script to force a release
    - another cruft function to retest semantic release

--- scripts/release_tools.py
+++ scripts/release_tools.py
@@ -16,3 +16,13 @@ def force_release_helper_beta():
     """
     print("Forcing a new release!")
     return True
+
+
+def force_release_helper_gamma():
+    """
+    This function exists solely to trigger a new release.
+    It doesn't do anything practical in the codebase.
+    One more small change to retest.
+    """
+    print("Forcing a new release!")
+    return True
