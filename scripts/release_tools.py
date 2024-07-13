bf710327ce1e7cd96b1db75556907673cb6776f4
mpazaryna <mpazbot@gmail.com>
2024-07-13 09:50:55 -0400

    feat: add force release helper function
    This commit is intended to trigger a new minor version release.
    The added function doesn't affect any actual functionality.

---
+++ scripts/release_tools.py
@@ -0,0 +1,7 @@
+def force_release_helper():
+    """
+    This function exists solely to trigger a new release.
+    It doesn't do anything practical in the codebase.
+    """
+    print("Forcing a new release!")
+    return True
