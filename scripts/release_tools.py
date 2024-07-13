36dd72b22acc0ebbd3a88a2d5df7916b37c3d120
mpazaryna <mpazbot@gmail.com>
2024-07-13 09:55:29 -0400

    fix: update with method to retest
    - added a new method, previous commit did not trigger a release.

--- scripts/release_tools.py
+++ scripts/release_tools.py
@@ -6,3 +6,13 @@ def force_release_helper():
     """
     print("Forcing a new release!")
     return True
+
+
+def force_release_helper_beta():
+    """
+    This function exists solely to trigger a new release.
+    It doesn't do anything practical in the codebase.
+    One more small change to retest.
+    """
+    print("Forcing a new release!")
+    return True
